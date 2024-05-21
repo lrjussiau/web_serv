a2ba43bf4cafe60a7757de6e16ef29fbda0c7535
lrjussiau <lr.jussiaume@gmail.com>
2024-05-21 09:28:06 +0200

    init canonical form

--- src/Response.cpp
+++ src/Response.cpp
@@ -3,3 +3,19 @@
 Response::Response(void) {
 	return;
 }
+
+Response::Response(Client client) {
+
+	
+	return;
+}
+
+Response::~Response(void) {
+	return;
+}
+
+Response::Response(const Response& src) {
+	if (this != &src)
+		*this = src;
+	return;
+}
